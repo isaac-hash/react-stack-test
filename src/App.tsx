cdd5f8d5d30c36df30380d854ad1b8cfee428d4b
Isaac Chukwudulue <116913165+isaac-hash@users.noreply.github.com>
2026-05-01 20:34:17 +0100

    Update API URL to new endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,8 @@ function App() {
       return apiPath.replace(/\/$/, '')
     }
     // return (import.meta.env.VITE_API_URL || 'http://77.68.50.228:3015').replace(/\/$/, '')
-    return ('http://77.68.50.228:3015').replace(/\/$/, '')
+    // return ('http://77.68.50.228:3015').replace(/\/$/, '')
+    return ('http://77.68.54.204:3013/').replace(/\/$/, '')
   }, [])
 
   const [tasks, setTasks] = useState<Task[]>([])
